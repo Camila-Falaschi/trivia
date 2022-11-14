216444a5f024d10c7d81dfa3cacf272c6abef044
Camila <falaschi.camila@yahoo.com.br>
2022-11-14 16:40:44 -0300

    Updates

---
+++ precache-manifest.41084cbc10a7744dcdfe3f6b865d2a66.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b4c9e14035b21d3c92cc79e36191d6",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "95edf996b944ac33ee17",
+    "url": "/trivia/static/css/main.48ac4834.chunk.css"
+  },
+  {
+    "revision": "4754ae0a2ead3c231ec1",
+    "url": "/trivia/static/js/2.99ddf2e6.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/trivia/static/js/2.99ddf2e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95edf996b944ac33ee17",
+    "url": "/trivia/static/js/main.88756d6c.chunk.js"
+  },
+  {
+    "revision": "13da866554f636ecdfac",
+    "url": "/trivia/static/js/runtime-main.62eca463.js"
+  },
+  {
+    "revision": "30f7918f8d5d0d27163a9a24b753a29d",
+    "url": "/trivia/static/media/couldBeBetter.30f7918f.gif"
+  },
+  {
+    "revision": "3c874ad3ea845dfcb9cca82f92bf4109",
+    "url": "/trivia/static/media/feedback0.3c874ad3.gif"
+  },
+  {
+    "revision": "bdd9b986c0306209dd67b12729cf3f4a",
+    "url": "/trivia/static/media/logoLogin.bdd9b986.png"
+  },
+  {
+    "revision": "bdc313097fbbc2bf995ffe3bbe236cec",
+    "url": "/trivia/static/media/logoheader.bdc31309.png"
+  },
+  {
+    "revision": "ddaf06bd893b899ad053ab54be094f0c",
+    "url": "/trivia/static/media/superWin.ddaf06bd.gif"
+  },
+  {
+    "revision": "245d1cf8099226be3f883a527180c201",
+    "url": "/trivia/static/media/win3.245d1cf8.gif"
+  },
+  {
+    "revision": "a9eba6953b7f527413965c8aa45c5f07",
+    "url": "/trivia/static/media/win4.a9eba695.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/app-toot-name/precache-manifest.f3bb9d1be7d395434e967e40b5117f13.js"
+  "/trivia/precache-manifest.41084cbc10a7744dcdfe3f6b865d2a66.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/app-toot-name/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trivia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
